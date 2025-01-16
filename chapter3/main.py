4965adf6d80f1ae66c750c1fd9954d42a99c3602
mirqwa <kimrodrikwa@gmail.com>
2025-01-16 15:55:01 +0300

    plotting data

--- chapter3/main.py
+++ chapter3/main.py
@@ -8,3 +8,11 @@ capitals = capitals[capitals["FEATURECLA"] == "Admin-0 capital"]
 grat = gpd.read_file(
     "data/110m_physical/ne_110m_graticules_all/ne_110m_graticules_10.shp"
 )
+
+fig, ax = plt.subplots(figsize=(12, 10))
+
+world.plot(ax=ax, color="lightgray")
+capitals.plot(ax=ax, color="black", markersize=10, marker="o")
+grat.plot(ax=ax, color="lightgray", linewidth=0.5)
+
+plt.show()
