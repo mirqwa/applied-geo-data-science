7dbb0f6adec9e55827e8529b921bacc7131d9e87
mirqwa <kimrodrikwa@gmail.com>
2025-01-16 15:45:24 +0300

    read the shapefiles

--- chapter3/main.py
+++ chapter3/main.py
@@ -3,4 +3,8 @@ import matplotlib.pyplot as plt
 
 
 world = gpd.read_file("data/110m_cultural/ne_110m_admin_0_countries.shp")
-breakpoint()
+capitals = gpd.read_file("data/110m_cultural/ne_110m_populated_places.shp")
+capitals = capitals[capitals["FEATURECLA"] == "Admin-0 capital"]
+grat = gpd.read_file(
+    "data/110m_physical/ne_110m_graticules_all/ne_110m_graticules_10.shp"
+)
