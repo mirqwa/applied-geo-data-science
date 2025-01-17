c73b3125bf7dd4adf5b9254e36896f8be99683de
mirqwa <kimrodrikwa@gmail.com>
2025-01-17 09:15:27 +0300

    filter by bounding box

--- chapter4/main.py
+++ chapter4/main.py
@@ -9,10 +9,16 @@ if __name__ == "__main__":
         "data/110m_cultural/ne_110m_populated_places.shp", mask=africa
     )
 
+    bounding_box = (-170, 70, -60, 10)
+    north_america_cities = gpd.read_file(
+        "data/110m_cultural/ne_110m_populated_places.shp", bbox=bounding_box
+    )
+
     fig, ax = plt.subplots(figsize=(12, 10))
 
     world.plot(ax=ax, color="lightgray")
     africa_capitals.plot(ax=ax, color="black", markersize=10, marker="o")
+    north_america_cities.plot(ax=ax, color="red", markersize=10, marker="o")
 
     ax.set(
         xlabel="Longitude(Degrees)", ylabel="Latitude(Degrees)", title="WGS 1984 Datum"
