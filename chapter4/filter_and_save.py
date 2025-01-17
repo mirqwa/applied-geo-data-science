2dd849317b2c44d0c3587bd3d1946f93ed3aa3d4
mirqwa <kimrodrikwa@gmail.com>
2025-01-17 09:53:43 +0300

    get south american cities

--- chapter4/filter_and_save.py
+++ chapter4/filter_and_save.py
@@ -30,11 +30,22 @@ if __name__ == "__main__":
         "GeoJSON",
     )
 
+    bounding_box = world[world["CONTINENT"] == "South America"].geometry.total_bounds
+    south_america_cities = gpd.read_file(
+        "data/110m_cultural/ne_110m_populated_places.shp", bbox=tuple(bounding_box)
+    )
+    save_geodf(
+        south_america_cities,
+        "data/output/south_america/capitals/capitals.geojson",
+        "GeoJSON",
+    )
+
     fig, ax = plt.subplots(figsize=(12, 10))
 
     world.plot(ax=ax, color="lightgray")
-    africa_capitals.plot(ax=ax, color="black", markersize=10, marker="o")
+    africa_capitals.plot(ax=ax, color="blue", markersize=10, marker="o")
     north_america_cities.plot(ax=ax, color="red", markersize=10, marker="o")
+    south_america_cities.plot(ax=ax, color="green", markersize=10, marker="o")
 
     ax.set(
         xlabel="Longitude(Degrees)", ylabel="Latitude(Degrees)", title="WGS 1984 Datum"
