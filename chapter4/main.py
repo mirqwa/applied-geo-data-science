0d2cb20bac39c123fd4254c91fda6a2c56f625dd
mirqwa <kimrodrikwa@gmail.com>
2025-01-17 09:30:44 +0300

    save to shapefile and geojson

--- chapter4/main.py
+++ chapter4/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
@@ -8,11 +10,17 @@ if __name__ == "__main__":
     africa_capitals = gpd.read_file(
         "data/110m_cultural/ne_110m_populated_places.shp", mask=africa
     )
+    output_path = Path("data/output/africa/capitals/capitals.shp")
+    Path(output_path.parent).mkdir(parents=True, exist_ok=True)
+    africa_capitals.to_file(output_path)
 
     bounding_box = (-170, 70, -60, 10)
     north_america_cities = gpd.read_file(
         "data/110m_cultural/ne_110m_populated_places.shp", bbox=bounding_box
     )
+    output_path = Path("data/output/north_america/capitals/capitals.geojson")
+    Path(output_path.parent).mkdir(parents=True, exist_ok=True)
+    north_america_cities.to_file(output_path, driver="GeoJSON")
 
     fig, ax = plt.subplots(figsize=(12, 10))
 
