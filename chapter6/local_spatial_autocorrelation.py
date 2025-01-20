2ce6c273b5ec654870e65f849b7e8b6d28a5b0e2
mirqwa <kimrodrikwa@gmail.com>
2025-01-20 14:51:20 +0300

    plot choropleths

--- chapter6/local_spatial_autocorrelation.py
+++ chapter6/local_spatial_autocorrelation.py
@@ -1,8 +1,10 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 from pysal.explore import esda
 from pysal.lib import weights
+from splot import esda as esdaplot
 
 import utils
 
@@ -29,13 +31,40 @@ def plot_choropleth(data: gpd.GeoDataFrame, price_lisa) -> None:
     plt.show()
 
 
-def compute_and_plot_local_moran(y: gpd.GeoDataFrame, w: weights.weights) -> None:
-    price_lisa = esda.moran.Moran_Local(y, w)
+def plot_choropleth_with_quadrant_classes(data, price_lisa) -> None:
+    _, ax = plt.subplots(1, figsize=(10, 10))
+    esdaplot.lisa_cluster(price_lisa, data, p=1, ax=ax)
+    plt.show()
+
+
+def plot_choropleth_with_signicance(data: gpd.GeoDataFrame, price_lisa) -> None:
+    _, ax = plt.subplots(1, figsize=(10, 10))
+    alpha = 0.05
+    labels = pd.Series(1 * (price_lisa.p_sim < alpha), index=data.index).map(
+        {1: "Significant", 0: "Insignificant"}
+    )
+    data.assign(ML_Sig=labels).plot(
+        column="ML_Sig",
+        categorical=True,
+        k=2,
+        cmap="vlag",
+        linewidth=0.1,
+        edgecolor="white",
+        legend=True,
+        ax=ax,
+    )
+    plt.show()
+
+
+def compute_and_plot_local_moran(data: gpd.GeoDataFrame, w: weights.weights) -> None:
+    price_lisa = esda.moran.Moran_Local(data["price"], w)
     kde_plot(price_lisa)
     plot_choropleth(data, price_lisa)
+    plot_choropleth_with_quadrant_classes(data, price_lisa)
+    plot_choropleth_with_signicance(data, price_lisa)
 
 
 if __name__ == "__main__":
     data = utils.get_data()
     data, w = utils.calculate_weight_and_lag(data, "price")
-    compute_and_plot_local_moran(data["price"], w)
+    compute_and_plot_local_moran(data, w)
