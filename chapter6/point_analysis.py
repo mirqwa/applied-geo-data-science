e3569e86bd5b00c0e1bdc34e6ba58b81d55ef555
mirqwa <kimrodrikwa@gmail.com>
2025-01-27 13:36:23 +0300

    plot places of worship

--- chapter6/point_analysis.py
+++ chapter6/point_analysis.py
@@ -1,5 +1,15 @@
+import contextily as cx
 import geopandas as gpd
+import matplotlib.pyplot as plt
 import pandas as pd
+
+
+def plot_places_of_worship(places_of_worship_gdf: gpd.GeoDataFrame) -> None:
+    places_of_worship_wm = places_of_worship_gdf.to_crs(epsg=3857)
+    ax = places_of_worship_wm.plot(figsize=(10, 10), alpha=0.5, edgecolor="k")
+    cx.add_basemap(ax, crs=places_of_worship_wm.crs, zoom=12)
+    ax.set_axis_off()
+    plt.show()
 
 
 def get_places_of_worship_gdf() -> gpd.GeoDataFrame:
@@ -14,3 +24,4 @@ def get_places_of_worship_gdf() -> gpd.GeoDataFrame:
 
 if __name__ == "__main__":
     places_of_worship_gdf = get_places_of_worship_gdf()
+    plot_places_of_worship(places_of_worship_gdf)
