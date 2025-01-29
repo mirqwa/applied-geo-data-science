7278f182d7d91426766d9735c2b66740205dabb6
mirqwa <kimrodrikwa@gmail.com>
2025-01-29 12:25:47 +0300

    calculate morans i

--- chapter8/exploratory_data_analysis.py
+++ chapter8/exploratory_data_analysis.py
@@ -1,7 +1,15 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from esda.moran import Moran
+from libpysal.weights import Queen
 
 import constants
+
+
+np.random.seed(32)
 
 
 def plot_data(ny_census: gpd.GeoDataFrame) -> None:
@@ -29,6 +37,18 @@ def plot_data(ny_census: gpd.GeoDataFrame) -> None:
     plt.show()
 
 
+def calculate_moran_i(ny_census: gpd.GeoDataFrame) -> None:
+    w = Queen.from_dataframe(ny_census)
+    moransi_results = [Moran(ny_census[v], w) for v in constants.GEO_DEMO_RN]
+    moransi_results = [
+        (v, res.I, res.p_sim) for v, res in zip(constants.GEO_DEMO_RN, moransi_results)
+    ]
+    table = pd.DataFrame(
+        moransi_results, columns=["GEODEMO Var", "Moran's I", "P-value"]
+    ).set_index("GEODEMO Var")
+
+
 if __name__ == "__main__":
     ny_census = gpd.read_file("data/us_census/ny_census_transformed.geojson")
     plot_data(ny_census)
+    calculate_moran_i(ny_census)
