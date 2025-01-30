e0f882118114b057d893cb548dac2bfb559b0487
mirqwa <kimrodrikwa@gmail.com>
2025-01-30 10:02:00 +0300

    specify cluster column names

--- chapter8/agglomerative_hierarchical_clustering.py
+++ chapter8/agglomerative_hierarchical_clustering.py
@@ -12,28 +12,30 @@ import utils
 np.random.seed(32)
 
 
-def fit_model(ny_census: gpd.GeoDataFrame, w: weights.weights = None) -> None:
+def fit_model(
+    ny_census: gpd.GeoDataFrame, label_column_name: str, w: weights.weights = None
+) -> None:
     model = (
         AgglomerativeClustering(linkage="ward", connectivity=w.sparse, n_clusters=5)
         if w
         else AgglomerativeClustering(linkage="ward", n_clusters=5)
     )
     model.fit(ny_census[constants.GEO_DEMO_RN])
-    ny_census["ward5_label"] = model.labels_
-    ward5sizes = ny_census.groupby("ward5_label").size()
-    utils.plot_clusters_choropleth(ny_census, "ward5_label", "Set3")
+    ny_census[label_column_name] = model.labels_
+    ward5sizes = ny_census.groupby(label_column_name).size()
+    utils.plot_clusters_choropleth(ny_census, label_column_name, "Set3")
     utils.plot_radial_plot(
-        ny_census.groupby("ward5_label")[constants.GEO_DEMO_RN].mean()
+        ny_census.groupby(label_column_name)[constants.GEO_DEMO_RN].mean()
     )
 
 
 if __name__ == "__main__":
     ny_census = gpd.read_file("data/us_census/ny_census_transformed_and_scaled.geojson")
-    # fit_model(ny_census.copy())
+    fit_model(ny_census, "ward5_label")
 
     # spatially constrained clustering
     spatial_w = Queen.from_dataframe(ny_census)
-    fit_model(ny_census.copy(), spatial_w)
+    fit_model(ny_census, "ward5wgt_label", spatial_w)
 
     knn_spatial_w = KNN.from_dataframe(ny_census, k=10)
-    fit_model(ny_census.copy(), knn_spatial_w)
+    fit_model(ny_census, "ward5_knnwgt_label", knn_spatial_w)
