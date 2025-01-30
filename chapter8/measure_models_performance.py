0f0c660c1eaa18c3a64528f41b22e47e62697689
mirqwa <kimrodrikwa@gmail.com>
2025-01-30 10:28:45 +0300

    get models performance measures

--- chapter8/measure_models_performance.py
+++ chapter8/measure_models_performance.py
@@ -1,7 +1,14 @@
 import geopandas as gpd
+import pandas as pd
 
 from libpysal.weights import Queen, KNN
+from sklearn.metrics import (
+    calinski_harabasz_score,
+    davies_bouldin_score,
+    silhouette_score,
+)
 
+import constants
 from k_means_clustering import fit_model as fit_k_means_model
 from agglomerative_hierarchical_clustering import fit_model as fit_ahc_model
 
@@ -17,5 +24,42 @@ def fit_models() -> gpd.GeoDataFrame:
     return ny_census
 
 
+def get_performance_values(ny_census: gpd.GeoDataFrame) -> None:
+    ch_scores = []
+    db_scores = []
+    s_scores = []
+    for model in (
+        "kmeans_5_label",
+        "ward5_label",
+        "ward5wgt_label",
+        "ward5_knnwgt_label",
+    ):
+        ch_score = calinski_harabasz_score(
+            ny_census[constants.GEO_DEMO_RN],
+            ny_census[model],
+        )
+        ch_scores.append((model, ch_score))
+        # compute the DB score
+        db_score = davies_bouldin_score(
+            ny_census[constants.GEO_DEMO_RN],
+            ny_census[model],
+        )
+        db_scores.append((model, db_score))
+        # compute the silhouette score
+        s_score = silhouette_score(
+            ny_census[constants.GEO_DEMO_RN],
+            ny_census[model],
+        )
+        s_scores.append((model, s_score))
+    ch_df = pd.DataFrame(ch_scores, columns=["model", "CH score"]).set_index("model")
+    db_df = pd.DataFrame(db_scores, columns=["model", "DB score"]).set_index("model")
+    s_df = pd.DataFrame(s_scores, columns=["model", "Silhouettescore"]).set_index(
+        "model"
+    )
+    scores_df = ch_df.merge(db_df, on="model")
+    scores_df = scores_df.merge(s_df, on="model")
+
+
 if __name__ == "__main__":
     ny_census = fit_models()
+    get_performance_values(ny_census)
