9dc849be19b2f274162f187e3dd98ddf4d158859
mirqwa <kimrodrikwa@gmail.com>
2025-01-30 08:54:03 +0300

    add radial plot to utils

--- chapter8/k_means_clustering.py
+++ chapter8/k_means_clustering.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as go
 from sklearn.cluster import KMeans
 
 import constants
+import utils
 
 
 np.random.seed(0)
@@ -56,38 +57,15 @@ def calculate_tract_average_areas(ny_census: gpd.GeoDataFrame) -> None:
     plt.show()
 
 
-def plot_radial_plot(ny_census: gpd.GeoDataFrame) -> None:
-    k5means_s = ny_census.groupby("kmeans_5_label")[constants.GEO_DEMO_RN].mean()
-    k5means_s.round(2)
-    categories = k5means_s.columns
-    fig = go.Figure()
-    for g in k5means_s.index:
-        fig.add_trace(
-            go.Scatterpolar(
-                r=k5means_s.loc[g].values,
-                theta=categories,
-                fill="toself",
-                name=f"cluster #{g}",
-            )
-        )
-    fig.update_layout(
-        polar=dict(
-            radialaxis=dict(visible=True, range=[-2, 5])  # here we can define the range
-        ),
-        showlegend=True,
-        title="Cluster Radial Plot",
-        title_x=0.5,
-    )
-    fig.show()
-
-
 def get_and_plot_clusters(ny_census: gpd.GeoDataFrame) -> None:
     kmeans = KMeans(n_clusters=5)
     kmeans_5 = kmeans.fit(ny_census[constants.GEO_DEMO_RN])
     ny_census["kmeans_5_label"] = kmeans_5.labels_
     plot_clusters(ny_census)
     calculate_tract_average_areas(ny_census)
-    plot_radial_plot(ny_census)
+    utils.plot_radial_plot(
+        ny_census.groupby("kmeans_5_label")[constants.GEO_DEMO_RN].mean()
+    )
 
 
 if __name__ == "__main__":
---
+++ chapter8/utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def plot_radial_plot(cluster_means: pd.DataFrame) -> None:
+    cluster_means.round(2)
+    categories = cluster_means.columns
+    fig = go.Figure()
+    for g in cluster_means.index:
+        fig.add_trace(
+            go.Scatterpolar(
+                r=cluster_means.loc[g].values,
+                theta=categories,
+                fill="toself",
+                name=f"cluster #{g}",
+            )
+        )
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(visible=True, range=[-2, 5])  # here we can define the range
+        ),
+        showlegend=True,
+        title="Cluster Radial Plot",
+        title_x=0.5,
+    )
+    fig.show()
