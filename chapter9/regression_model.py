a37d53e5e0c723eb5f5f16f2022091418b437dd6
mirqwa <kimrodrikwa@gmail.com>
2025-01-30 11:08:27 +0300

    one hot encode room types

--- chapter9/regression_model.py
+++ chapter9/regression_model.py
@@ -1,5 +1,34 @@
 import geopandas as gpd
+import numpy as np
+
+
+def one_hot_encode_room_types(manhattan_listings: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    manhattan_listings["rt_entire_home_apartment"] = np.where(
+        manhattan_listings["room_type"] == "Entire home/apt", 1, 0
+    )
+    manhattan_listings["rt_private_room"] = np.where(
+        manhattan_listings["room_type"] == "Private room", 1, 0
+    )
+    manhattan_listings["rt_shared_room"] = np.where(
+        manhattan_listings["room_type"] == "Shared room", 1, 0
+    )
+    return manhattan_listings
+
+
+def get_train_data() -> gpd.GeoDataFrame:
+    manhattan_listings = gpd.read_file("data/new_york/manhattan_listings.geojson")
+    variables = [
+        "id",  # Unique identifier for the listing
+        "room_type",  # Type of room
+        "accommodates",  # The maximum capacity of the listing
+        "bedrooms",  # The number of bedrooms
+        "beds",  # The number of beds
+        "review_scores_rating",  # The rating
+        "price",  # The nightly rental rate, dependent variable (Y)
+    ]
+    manhattan_listings = manhattan_listings[variables]
+    manhattan_listings = one_hot_encode_room_types(manhattan_listings)
 
 
 if __name__ == "__main__":
-    manhattan_listings = gpd.read_file("data/new_york/manhattan_listings.geojson")
+    manhattan_listings = get_train_data()
