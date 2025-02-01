fabdf60c1ccdefeba04e66fec127dcd4a20c7c5f
mirqwa <kimrodrikwa@gmail.com>
2025-02-01 11:30:45 +0300

    get networ data

--- chapter10/lscp.py
+++ chapter10/lscp.py
@@ -1,4 +1,19 @@
+from pathlib import Path
+import geopandas as gpd
+import osmnx as ox
 import pulp
+
+
+def get_network_data() -> tuple[gpd.GeoDataFrame]:
+    nodes_path = "data/washington/network_nodes.geojson"
+    edges_path = "data/washington/network_edges.geojson"
+    if Path(nodes_path).is_file() and Path(edges_path).is_file():
+        return gpd.read_file(nodes_path), gpd.read_file(edges_path)
+    G = ox.graph_from_place("Washington, DC", network_type="drive")
+    gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+    gdf_nodes.to_file(nodes_path, driver="GeoJSON")
+    gdf_edges.to_file(edges_path, driver="GeoJSON")
+    return gdf_nodes, gdf_edges
 
 
 if __name__ == "__main__":
@@ -10,3 +25,4 @@ if __name__ == "__main__":
     medical_centers_seed = 54321
 
     solver = pulp.PULP_CBC_CMD(msg=False, warmStart=True)
+    gdf_nodes, gdf_edges = get_network_data()
