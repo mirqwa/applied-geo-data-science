dfb9e479fbc914aa77baa0ddf05ba609eba9dab5
mirqwa <kimrodrikwa@gmail.com>
2025-02-01 11:55:48 +0300

    plot nodes and edges

--- chapter10/lscp.py
+++ chapter10/lscp.py
@@ -1,7 +1,23 @@
 from pathlib import Path
+
+import contextily as cx
 import geopandas as gpd
+import matplotlib.pyplot as plt
 import osmnx as ox
 import pulp
+
+
+def plot_nodes_and_edges(
+    gdf_nodes: gpd.GeoDataFrame, gdf_edges: gpd.GeoDataFrame
+) -> None:
+    _, ax = plt.subplots(figsize=(15, 15))
+    gdf_nodes_wm = gdf_nodes.to_crs(epsg=3857)
+    gdf_nodes_wm.plot(ax=ax, alpha=0.5, edgecolor="k")
+    gdf_edges_wm = gdf_edges.to_crs(epsg=3857)
+    gdf_edges_wm.plot(ax=ax, alpha=0.5, color="red", edgecolor="k")
+    cx.add_basemap(ax, crs=gdf_nodes_wm.crs, zoom=12)
+    ax.set_axis_off()
+    plt.show()
 
 
 def get_network_data(use_local_data: bool = False) -> tuple[gpd.GeoDataFrame]:
@@ -16,6 +32,13 @@ def get_network_data(use_local_data: bool = False) -> tuple[gpd.GeoDataFrame]:
     return gdf_nodes, gdf_edges
 
 
+def get_edges_subset(gdf_edges: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    gdf_edges.reset_index(inplace=True)
+    DC_BGs = gpd.read_file(
+        "data/tiger/TIGER2019/tl_2019_11_tract.zip", compression="zip"
+    )
+
+
 if __name__ == "__main__":
     patients = 150
     medical_centers = 4
@@ -25,4 +48,6 @@ if __name__ == "__main__":
     medical_centers_seed = 54321
 
     solver = pulp.PULP_CBC_CMD(msg=False, warmStart=True)
-    gdf_nodes, gdf_edges = get_network_data()
+    gdf_nodes, gdf_edges = get_network_data(True)
+    plot_nodes_and_edges(gdf_nodes, gdf_edges)
+    gdf_edges_clipped_p = get_edges_subset(gdf_edges)
