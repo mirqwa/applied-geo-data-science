f0ad20145e35a307d5a5b9e1c4bb93c311566523
mirqwa <kimrodrikwa@gmail.com>
2025-02-03 08:44:47 +0300

    get vrp distances

--- chapter10/vrp.py
+++ chapter10/vrp.py
@@ -1,9 +1,17 @@
+import argparse
+
 import utils
 
 
-def main():
+def main(api_key: str) -> None:
+    g_maps_client = utils.get_gmaps_client(api_key)
     data_gdf = utils.generate_data_for_vrp()
+    distances = utils.get_origin_destination_cost_matrix(data_gdf, g_maps_client, True)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_key", type=str, required=True)
+
+    args = parser.parse_args()
+    main(args.api_key)
