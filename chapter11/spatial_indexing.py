15273fa1b0b30cfc342aac25ede1f2dc597f4240
mirqwa <kimrodrikwa@gmail.com>
2025-02-03 15:19:22 +0300

    get manhattan boroughs

--- chapter11/spatial_indexing.py
+++ chapter11/spatial_indexing.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import pandas as pd
 
 
-def get_listings():
+def get_listings() -> gpd.GeoDataFrame:
     listings_df = pd.read_csv("data/listings.csv.gz")
     listings_gdf = gpd.GeoDataFrame(
         listings_df,
@@ -12,5 +12,13 @@ def get_listings():
     return listings_gdf
 
 
+def get_manhattan_boroughs() -> gpd.GeoDataFrame:
+    boroughs = gpd.read_file("data/new_york/nybb.shp")
+    manhattan_boroughs = boroughs[boroughs["BoroName"] == "Manhattan"]
+    manhattan_boroughs = manhattan_boroughs.to_crs("EPSG:4326")
+    return manhattan_boroughs
+
+
 if __name__ == "__main__":
     listings_gdf = get_listings()
+    manhattan_boroughs = get_manhattan_boroughs()
