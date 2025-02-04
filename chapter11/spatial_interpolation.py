6dd4c64bdb3ccc9ce0f125b5e0e100565605234c
mirqwa <kimrodrikwa@gmail.com>
2025-02-04 10:34:12 +0300

    filter daily data and get temp mean

--- chapter11/spatial_interpolation.py
+++ chapter11/spatial_interpolation.py
@@ -10,6 +10,9 @@ def get_weather_data():
     for weather_file in weather_files:
         dfs.append(pd.read_csv(f"{weather_dir}/{weather_file}"))
     weather_df = pd.concat(dfs)
+    weather_df = weather_df[weather_df["Date"] == "2023-10-01"]
+    weather_df = weather_df.dropna()
+    mean_temperature_df = weather_df.groupby(['Longitude', 'Latitude'])["Temperature"].mean().reset_index()
 
 
 if __name__ == "__main__":
