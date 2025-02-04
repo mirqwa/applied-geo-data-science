039f1dec4fdc367468101c784d351ebedd07f281
mirqwa <kimrodrikwa@gmail.com>
2025-02-04 11:49:36 +0300

    plot weather stations

--- chapter11/spatial_interpolation.py
+++ chapter11/spatial_interpolation.py
@@ -1,7 +1,23 @@
 import os
 
+import contextily as cx
 import geopandas as gpd
+import matplotlib.pyplot as plt
 import pandas as pd
+
+
+def plot_average_temperature(mean_temperature_gdf: gpd.GeoDataFrame) -> None:
+    _, ax = plt.subplots(1, figsize=(15, 15))
+    mean_temperature_gdf_wm = mean_temperature_gdf.to_crs(epsg=3857)
+    mean_temperature_gdf_wm.plot(ax=ax, alpha=0.5, edgecolor="k")
+    cx.add_basemap(
+        ax,
+        crs=mean_temperature_gdf_wm.crs,
+        zoom=16,
+        source=cx.providers.OpenStreetMap.Mapnik,
+    )
+    ax.set_axis_off()
+    plt.show()
 
 
 def get_weather_data():
@@ -25,6 +41,7 @@ def get_weather_data():
         ),
         crs="EPSG:4326",
     )
+    plot_average_temperature(mean_temperature_gdf)
 
 
 if __name__ == "__main__":
