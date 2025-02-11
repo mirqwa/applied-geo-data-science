584ec8f9b2e4f17c121b6a85c676eef43c2325d6
mirqwa <kimrodrikwa@gmail.com>
2025-02-11 13:49:08 +0300

    get google maps client

---
+++ chapter10/optimum_route.py
@@ -0,0 +1,15 @@
+import argparse
+
+import utils
+
+
+def main(api_key: str) -> None:
+    g_maps_client = utils.get_gmaps_client(api_key)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_key", type=str, required=True)
+
+    args = parser.parse_args()
+    main(args.api_key)
