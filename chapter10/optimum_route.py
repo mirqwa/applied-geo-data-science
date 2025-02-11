b5433f82d4b2dd9d74a7c25664b7a26b416b479a
mirqwa <kimrodrikwa@gmail.com>
2025-02-11 13:57:16 +0300

    geocode location

--- chapter10/optimum_route.py
+++ chapter10/optimum_route.py
@@ -5,6 +5,8 @@ import utils
 
 def main(api_key: str) -> None:
     g_maps_client = utils.get_gmaps_client(api_key)
+    geocode_result = g_maps_client.geocode("Kitale")
+    location = geocode_result[0]["geometry"]["location"]
 
 
 if __name__ == "__main__":
